87b565b8348ef1e6cb2d5a70ab3c0c71adc86462
Yuya Minami <yuya373@me.com>
2017-08-06 17:46:44 +0900

    chapter 4 preprocessor

--- prompt.c
+++ prompt.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#ifdef _WIN32
+#include <string.h>
+static char buffer[2048];
+
+char *readline(char *prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char *cpy = malloc(strlen(buffer) + 1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy) - 1] = '\0';
+  return cpy;
+}
+
+void add_history(char *unused) {}
+#else
 /* yaourt libedit */
 /* cc -std=c99 -Wall -ledit prompt.c -o prompt */
 #include <editline/readline.h>
+#endif
 
 int main(int argc, char **argv) {
   puts("Lispy Version 0.0.0.0.1");
