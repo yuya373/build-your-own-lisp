69401d5f3ff3e665d5f497132bd63bfa8bd485ba
Yuya Minami <yuya373@me.com>
2017-08-08 01:06:48 +0900

    chapter6 parsing user input

--- parsing.c
+++ parsing.c
@@ -25,13 +25,31 @@ void add_history(char *unused) {}
 #endif
 
 int main(int argc, char **argv) {
+  mpc_parser_t *Number = mpc_new("number");
+  mpc_parser_t *Operator = mpc_new("operator");
+  mpc_parser_t *Expr = mpc_new("expr");
+  mpc_parser_t *Lispy = mpc_new("lispy");
+  mpca_lang(MPCA_LANG_DEFAULT, "number : /-?[0-9]+/ ; \
+             operator: '+' | '-' | '*' | '/' ; \
+             expr: <number> | '(' <operator> <expr>+ ')' ; \
+             lispy: /^/ <operator> <expr>+ /$/ ; \
+            ",
+            Number, Operator, Expr, Lispy);
+
   puts("Lispy Version 0.0.0.0.1");
   puts("Press Ctrl+c to Exit\n");
 
   /* not 0, infinit loop */
   while (1) {
-    /* http://en.cppreference.com/ */
+    mpc_result_t r;
     char *input = readline("lispy> ");
+    if (mpc_parse("<stdin>", input, Lispy, &r)) {
+      mpc_ast_print((mpc_ast_t *)r.output);
+      mpc_ast_delete((mpc_ast_t *)r.output);
+    } else {
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
     add_history(input);
 
     puts(input);
@@ -39,5 +57,6 @@ int main(int argc, char **argv) {
     free(input);
   }
 
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
   return 0;
 }
