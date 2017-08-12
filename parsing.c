a1cfa5a191921a19154b619534eaf811ec041d23
Yuya Minami <yuya373@me.com>
2017-08-12 16:53:02 +0900

    chapter9 Parsing Expressions

--- parsing.c
+++ parsing.c
@@ -203,15 +203,17 @@ lval eval(mpc_ast_t *t) {
 
 int main(int argc, char **argv) {
   mpc_parser_t *Number = mpc_new("number");
-  mpc_parser_t *Operator = mpc_new("operator");
+  mpc_parser_t *Symbol = mpc_new("symbol");
+  mpc_parser_t *Sexpr = mpc_new("sexpr");
   mpc_parser_t *Expr = mpc_new("expr");
   mpc_parser_t *Lispy = mpc_new("lispy");
   mpca_lang(MPCA_LANG_DEFAULT, "number : /-?[0-9]+[\\.]?[0-9]*/ ; \
-             operator: '^' | '+' | '-' | '*' | '/' | '%' | /(add|sub|mul|div|min|max)/ ; \
-             expr: <number> | '(' <operator> <expr>+ ')' ; \
-             lispy: /^/ <operator> <expr>+ /$/ ; \
+             symbol: '^' | '+' | '-' | '*' | '/' | '%' | /(add|sub|mul|div|min|max)/ ; \
+             sexpr: '(' <expr>* ')' ; \
+             expr: <number> | <symbol> | <sexpr> ; \
+             lispy: /^/ <expr>* /$/ ; \
             ",
-            Number, Operator, Expr, Lispy);
+            Number, Symbol, Sexpr, Expr, Lispy);
 
   puts("Lispy Version 0.0.0.0.1");
   puts("Press Ctrl+c to Exit\n");
@@ -242,6 +244,6 @@ int main(int argc, char **argv) {
     free(input);
   }
 
-  mpc_cleanup(4, Number, Operator, Expr, Lispy);
+  mpc_cleanup(5, Number, Symbol, Sexpr, Expr, Lispy);
   return 0;
 }
