f0d0b805984090aa793e2e00b9ebf8e0ead1b75e
Yuya Minami <yuya373@me.com>
2017-08-12 19:31:34 +0900

    chapter9 Reading Expressions

--- parsing.c
+++ parsing.c
@@ -99,6 +99,50 @@ void lval_del(lval *v) {
   free(v);
 }
 
+lval *lval_read_num(mpc_ast_t *t) {
+  errno = 0;
+  double x = strtod(t->contents, NULL);
+  return errno != ERANGE ? lval_num(x) : lval_err((char *)"invalid number");
+}
+
+lval *lval_add(lval *v, lval *x) {
+  v->count++;
+  v->cell = (struct lval **)realloc(v->cell, (sizeof(lval *) * v->count));
+  v->cell[v->count - 1] = x;
+  return v;
+}
+
+lval *lval_read(mpc_ast_t *t) {
+  if (strstr(t->tag, "number")) {
+    return lval_read_num(t);
+  }
+  if (strstr(t->tag, "symbol")) {
+    return lval_sym(t->contents);
+  }
+  lval *x = NULL;
+  if (strcmp(t->tag, ">") == 0) {
+    x = lval_sexpr();
+  }
+  if (strstr(t->tag, "sexpr")) {
+    x = lval_sexpr();
+  }
+
+  for (int i = 0; i < t->children_num; i++) {
+    if (strcmp(t->children[i]->contents, "(") == 0) {
+      continue;
+    }
+    if (strcmp(t->children[i]->contents, ")") == 0) {
+      continue;
+    }
+    if (strcmp(t->children[i]->tag, "regex") == 0) {
+      printf("tag is regex: %s", t->children[i]->contents);
+      continue;
+    }
+    x = lval_add(x, lval_read(t->children[i]));
+  }
+  return x;
+}
+
 void lval_print(lval v) {
   switch (v.type) {
   case LVAL_NUM:
