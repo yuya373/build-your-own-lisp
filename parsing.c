37e93ba5a33a269f3fd1b3f45c404e49edf4b8a1
Yuya Minami <yuya373@me.com>
2017-08-13 00:25:08 +0900

    fix string symbol matching

--- parsing.c
+++ parsing.c
@@ -395,7 +395,8 @@ int main(int argc, char **argv) {
   mpc_parser_t *Expr = mpc_new("expr");
   mpc_parser_t *Lispy = mpc_new("lispy");
   mpca_lang(MPCA_LANG_DEFAULT, "number : /-?[0-9]+[\\.]?[0-9]*/ ; \
-             symbol: '^' | '+' | '-' | '*' | '/' | '%' | /(add|sub|mul|div|min|max)/ ; \
+             symbol: '^' | '+' | '-' | '*' | '/' | '%' | \
+                     \"add\" | \"sub\" | \"mul\" | \"div\" | \"min\" | \"max\" ; \
              sexpr: '(' <expr>* ')' ; \
              qexpr: '{' <expr>* '}' ; \
              expr: <number> | <symbol> | <sexpr> | <qexpr> ; \
