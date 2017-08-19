759a950d4f3e0235cf49a60ef1fe6d4041c8e692
Yuya Minami <yuya373@me.com>
2017-08-19 18:41:31 +0900

    chapter13 Ordering

--- parsing.c
+++ parsing.c
@@ -880,6 +880,34 @@ lval *builtin_lambda(lenv *e, lval *a) {
   return lval_lambda(formals, body);
 }
 
+lval *builtin_ord(lenv *e, lval *a, char *op) {
+  LASSERT_NUM(op, a, 2);
+  LASSERT_TYPE(op, a, 0, LVAL_NUM);
+  LASSERT_TYPE(op, a, 1, LVAL_NUM);
+
+  int r;
+  if (strcmp(op, ">") == 0) {
+    r = (a->cell[0]->num) > (a->cell[1]->num);
+  }
+  if (strcmp(op, "<") == 0) {
+    r = (a->cell[0]->num) < (a->cell[1]->num);
+  }
+  if (strcmp(op, ">=") == 0) {
+    r = (a->cell[0]->num) >= (a->cell[1]->num);
+  }
+  if (strcmp(op, "<=") == 0) {
+    r = (a->cell[0]->num) <= (a->cell[1]->num);
+  }
+
+  lval_del(a);
+  return lval_num(r);
+}
+
+lval *builtin_gt(lenv *e, lval *a) { return builtin_ord(e, a, (char *)">"); }
+lval *builtin_lt(lenv *e, lval *a) { return builtin_ord(e, a, (char *)"<"); }
+lval *builtin_gteq(lenv *e, lval *a) { return builtin_ord(e, a, (char *)">="); }
+lval *builtin_lteq(lenv *e, lval *a) { return builtin_ord(e, a, (char *)"<="); }
+
 void lenv_add_builtin(lenv *e, char *name, lbuiltin func) {
   lval *k = lval_sym(name);
   lval *v = lval_fun(func, name);
