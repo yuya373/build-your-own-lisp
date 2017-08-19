7702ef45086174c908893f57effc9c654a3460a0
Yuya Minami <yuya373@me.com>
2017-08-19 20:22:19 +0900

    chapter13 Equality

--- parsing.c
+++ parsing.c
@@ -908,6 +908,56 @@ lval *builtin_lt(lenv *e, lval *a) { return builtin_ord(e, a, (char *)"<"); }
 lval *builtin_gteq(lenv *e, lval *a) { return builtin_ord(e, a, (char *)">="); }
 lval *builtin_lteq(lenv *e, lval *a) { return builtin_ord(e, a, (char *)"<="); }
 
+int lval_eq(lval *x, lval *y) {
+  if (x->type != y->type) {
+    return 0;
+  }
+  switch (x->type) {
+  case LVAL_NUM:
+    return x->num == y->num;
+  case LVAL_ERR:
+    return strcmp(x->err, y->err) == 0;
+  case LVAL_SYM:
+    return strcmp(x->sym, y->sym) == 0;
+  case LVAL_FUN:
+    if (x->builtin || y->builtin) {
+      return x->builtin == y->builtin;
+    } else {
+      return lval_eq(x->formals, y->formals) && lval_eq(x->body, y->body);
+    }
+  case LVAL_QEXPR:
+  case LVAL_SEXPR:
+    if (x->count != y->count) {
+      return 0;
+    }
+    for (int i = 0; i < x->count; i++) {
+      if (!lval_eq(x->cell[i], y->cell[i])) {
+        return 0;
+      }
+    }
+    return 1;
+  }
+  return 0;
+}
+
+lval *builtin_cmp(lenv *e, lval *a, char *op) {
+  LASSERT_NUM(op, a, 2);
+
+  int r;
+  if (strcmp(op, "==") == 0) {
+    r = lval_eq(a->cell[0], a->cell[1]);
+  }
+  if (strcmp(op, "!=") == 0) {
+    r = !lval_eq(a->cell[0], a->cell[1]);
+  }
+
+  lval_del(a);
+  return lval_num(r);
+}
+
+lval *builtin_eq(lenv *e, lval *a) { return builtin_cmp(e, a, (char *)"=="); }
+lval *builtin_ne(lenv *e, lval *a) { return builtin_cmp(e, a, (char *)"!="); }
+
 void lenv_add_builtin(lenv *e, char *name, lbuiltin func) {
   lval *k = lval_sym(name);
   lval *v = lval_fun(func, name);
